de99a3e276131c8bff4d19cb1620a2e8a84ccfab
Sarah Ellem <89920953+SarahEllem@users.noreply.github.com>
2022-09-08 20:10:16 -0300

    commit

---
+++ Exercicios_CSharp/exercicio_calculadoraDeMulta/Program.cs
@@ -0,0 +1,31 @@
+﻿//Escreva um programa que pergunte a velocidade de um carro. 
+//Caso ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário foi multado. 
+//Nesse caso, exiba o valor da multa, cobrando R$5 por cada Km acima da velocidade permitida.
+
+namespace exercicio_calculadoraDeMulta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidadeCarro, multa, diferencaKm;
+
+            Console.Write("Digita a velocidade do carro: ");
+            velocidadeCarro = Double.Parse(Console.ReadLine());
+
+            if (velocidadeCarro > 80)
+            {
+                Console.WriteLine($"{velocidadeCarro}Km/h está acima do limite permitido. Você será multado.");
+
+                diferencaKm = velocidadeCarro - 80;
+                multa = diferencaKm * 5;
+
+                Console.WriteLine($"Sua multa é de R${multa}");
+            }
+            else
+            {
+                Console.WriteLine($"Sua velocidade de {velocidadeCarro} está dentro da velocidade permitida.");
+            }
+        }
+    }
+}
