717cc2be26ded994d81858822f82f6d7f5dda25f
Sarah Ellem <89920953+SarahEllem@users.noreply.github.com>
2022-09-09 12:45:23 -0300

    Add files via upload

---
+++ Exercicios_CSharp/exercicio_CalcTinta/Program.cs
@@ -0,0 +1,25 @@
+﻿//Faça um algoritmo que leia a largura e altura de uma parede, calcule e 
+//mostre a área a ser pintada e a quantidade de tinta necessária para o serviço,
+//sabendo que cada litro de tinta pinta uma área de 2metros quadrados.
+namespace exercicio_CalcTinta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, largura, tinta, area;
+
+            Console.Write("Digite o valor da altura: ");
+            altura = Double.Parse(Console.ReadLine());
+
+            Console.Write("Agora, digite o valor da largura: ");
+            largura = Double.Parse(Console.ReadLine());
+
+            area = altura * largura;
+            tinta = area / 2;
+
+            Console.WriteLine($"\nA sua área total é de {area} metros");
+            Console.WriteLine($"Você precisará de {tinta} litros");
+        }
+    }
+}
