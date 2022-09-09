8dba124c961151b873016e7b309eb0992e8f376a
Sarah Ellem <89920953+SarahEllem@users.noreply.github.com>
2022-09-09 12:44:25 -0300

    commit inicial

---
+++ Exercicios_CSharp/exercicio_numeroAntecSuces/Program.cs
@@ -0,0 +1,21 @@
+﻿//Faça um programa que leia um número inteiro e mostre o seu antecessor e seu 
+//sucessor.
+
+namespace exercicio_numeroAntecSuces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numAntecessor, numSucessor;
+
+            Console.WriteLine("Digite um número para saber seu antecessor e sucessor: ");
+            num = int.Parse(Console.ReadLine());
+
+            numAntecessor = num - 1;
+            numSucessor = num + 1;
+
+            Console.WriteLine($"O antecessor do seu número ({num}) é {numAntecessor} e o sucessor é {numSucessor}");
+        }
+    }
+}
