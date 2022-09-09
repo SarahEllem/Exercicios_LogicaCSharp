72b8188f92a603ae518c2ecacca0ff5f0cd8440e
Sarah Ellem <89920953+SarahEllem@users.noreply.github.com>
2022-09-08 21:08:33 -0300

    commit inicial

---
+++ Exercicios_CSharp/exercicio_calc-IdadeEleitoral/Program.cs
@@ -0,0 +1,41 @@
+﻿//Faça um programa que leia o ano de nascimento de uma pessoa, calcule sua idade e depois 
+//mostre se ela já pode ou não votar.
+namespace exercicio_calcIdadeEleitoral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNasc, anoAtual, idade, anosFaltantes;
+
+            Console.Write("Escreva o seu ano de nascimento: ");
+            anoNasc = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o ano atual: ");
+            anoAtual = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNasc;
+            Console.WriteLine($"Você tem {idade} anos.");
+
+            if (idade < 16)
+            {
+                Console.WriteLine("Você ainda não pode votar.");
+                anosFaltantes = 18 - idade;
+                Console.WriteLine($"\nFaltam {anosFaltantes} anos para o seu voto se tornar obrigatório.");
+            }
+            else if (idade >= 16 && idade < 18)
+            {
+                Console.WriteLine("Você já pode votar, mas é opcional.");
+                anosFaltantes = 18 - idade;
+                Console.WriteLine($"\nFaltam {anosFaltantes} anos para o seu voto se tornar obrigatório.");
+            }
+            else
+            {
+                Console.WriteLine("Você pode e DEVE votar.");
+                anosFaltantes = idade - 18;
+                Console.WriteLine($"\nVocê deveria votar a {anosFaltantes} anos.");
+            }
+        }
+    }
+}
+
